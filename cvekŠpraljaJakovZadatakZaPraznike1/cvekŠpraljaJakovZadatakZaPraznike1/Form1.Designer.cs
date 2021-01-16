274c40bf4c67c0d6e00854401ca84a1b50622370
Jakov764 <55740028+Jakov764@users.noreply.github.com>
2021-01-16 19:31:25 +0100

    Add files via upload

---
+++ cvekŠpraljaJakovZadatakZaPraznike1/cvekŠpraljaJakovZadatakZaPraznike1/Form1.Designer.cs
@@ -0,0 +1,201 @@
+﻿namespace cvekŠpraljaJakovZadatakZaPraznike1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.natragBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.naprijedBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.idiBtn = new System.Windows.Forms.Button();
+            this.gmailBtn = new System.Windows.Forms.Button();
+            this.webmailBtn = new System.Windows.Forms.Button();
+            this.ispisBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(-1, 59);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(1192, 590);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // natragBtn
+            // 
+            this.natragBtn.BackColor = System.Drawing.Color.Transparent;
+            this.natragBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("natragBtn.BackgroundImage")));
+            this.natragBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.natragBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.natragBtn.Location = new System.Drawing.Point(12, 16);
+            this.natragBtn.Name = "natragBtn";
+            this.natragBtn.Size = new System.Drawing.Size(30, 31);
+            this.natragBtn.TabIndex = 1;
+            this.natragBtn.UseVisualStyleBackColor = false;
+            this.natragBtn.Click += new System.EventHandler(this.natragBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Address:";
+            // 
+            // naprijedBtn
+            // 
+            this.naprijedBtn.BackColor = System.Drawing.Color.Transparent;
+            this.naprijedBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("naprijedBtn.BackgroundImage")));
+            this.naprijedBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.naprijedBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.naprijedBtn.Location = new System.Drawing.Point(48, 16);
+            this.naprijedBtn.Name = "naprijedBtn";
+            this.naprijedBtn.Size = new System.Drawing.Size(30, 31);
+            this.naprijedBtn.TabIndex = 4;
+            this.naprijedBtn.UseVisualStyleBackColor = false;
+            this.naprijedBtn.Click += new System.EventHandler(this.naprijedBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(139, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(653, 20);
+            this.textBox1.TabIndex = 5;
+            // 
+            // idiBtn
+            // 
+            this.idiBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("idiBtn.BackgroundImage")));
+            this.idiBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.idiBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.idiBtn.Location = new System.Drawing.Point(801, 15);
+            this.idiBtn.Name = "idiBtn";
+            this.idiBtn.Size = new System.Drawing.Size(29, 28);
+            this.idiBtn.TabIndex = 6;
+            this.idiBtn.UseVisualStyleBackColor = true;
+            this.idiBtn.Click += new System.EventHandler(this.idiBtn_Click);
+            // 
+            // gmailBtn
+            // 
+            this.gmailBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("gmailBtn.BackgroundImage")));
+            this.gmailBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.gmailBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.gmailBtn.Location = new System.Drawing.Point(886, 5);
+            this.gmailBtn.Name = "gmailBtn";
+            this.gmailBtn.Size = new System.Drawing.Size(45, 35);
+            this.gmailBtn.TabIndex = 7;
+            this.gmailBtn.UseVisualStyleBackColor = true;
+            this.gmailBtn.Click += new System.EventHandler(this.gmailBtn_Click);
+            // 
+            // webmailBtn
+            // 
+            this.webmailBtn.BackColor = System.Drawing.Color.Transparent;
+            this.webmailBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("webmailBtn.BackgroundImage")));
+            this.webmailBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.webmailBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.webmailBtn.Location = new System.Drawing.Point(961, 7);
+            this.webmailBtn.Name = "webmailBtn";
+            this.webmailBtn.Size = new System.Drawing.Size(106, 31);
+            this.webmailBtn.TabIndex = 8;
+            this.webmailBtn.UseVisualStyleBackColor = false;
+            this.webmailBtn.Click += new System.EventHandler(this.webmailBtn_Click);
+            // 
+            // ispisBtn
+            // 
+            this.ispisBtn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ispisBtn.BackgroundImage")));
+            this.ispisBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ispisBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ispisBtn.Location = new System.Drawing.Point(1096, 4);
+            this.ispisBtn.Name = "ispisBtn";
+            this.ispisBtn.Size = new System.Drawing.Size(50, 50);
+            this.ispisBtn.TabIndex = 9;
+            this.ispisBtn.UseVisualStyleBackColor = true;
+            this.ispisBtn.Click += new System.EventHandler(this.ispisBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(892, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Gmail";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(973, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Carnet Webmail";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1193, 648);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ispisBtn);
+            this.Controls.Add(this.webmailBtn);
+            this.Controls.Add(this.gmailBtn);
+            this.Controls.Add(this.idiBtn);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.naprijedBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.natragBtn);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Button natragBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button naprijedBtn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button idiBtn;
+        private System.Windows.Forms.Button gmailBtn;
+        private System.Windows.Forms.Button webmailBtn;
+        private System.Windows.Forms.Button ispisBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ cvekŠpraljaJakovZadatakZaPraznike1/cvekŠpraljaJakovZadatakZaPraznike1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cvekŠpraljaJakovZadatakZaPraznike1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void idiBtn_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate(textBox1.Text);
+        }
+
+        private void gmailBtn_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.gmail.com");
+        }
+
+        private void webmailBtn_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://webmail.com");
+        }
+
+        private void ispisBtn_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Print();
+        }
+
+        private void natragBtn_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoBack();
+        }
+
+        private void naprijedBtn_Click(object sender, EventArgs e)
+        {
+            webBrowser1.GoForward();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.google.hr");
+        }
+    }
+}
